096579bf05abbcc9a18dfb2a6454cc2f535b8690
Kushagra Kukreti <kushagra@xaigi.tech>
2024-06-17 22:16:34 +0530

    multer , cloudinary added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,43 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+export const uploadOnCloudinary  = async (localFilePath)=>{
+
+    try {
+        if(!localFilePath)return null;
+        //upload file on cloudinary 
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        console.log("File uploaded successfully",response.url);
+        return response;
+    } catch (error) {
+       fs.unlinkSync(localFilePath);
+       //remove from your localserver since upload got failed
+       return null;
+    }
+
+}
+
+
+
+//REFERENCE CODE FOR CLOUDINARY
+
+ 
+// const uploadResult = await cloudinary.uploader
+//   .upload(
+//     "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+//     {
+//       public_id: "shoes",
+//     }
+//   )
+//   .catch((error) => {
+//     console.log(error);
+//   });
